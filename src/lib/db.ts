d10238d5d5ced2c51adec4b26d859143e8185313
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-06-21 17:32:09 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,24 +1,3 @@
-// import mongoose, { mongo } from 'mongoose';
-// import { cache } from 'react';
-
-// const MONGODB_URI = process.env.MONGODB_URI || '';
-
-// if(!MONGODB_URI) throw new Error('Please define the mongodburi in .env.local');
-
-// let cached = (global as any).mongoose || {conn: null};
-
-
-// export async function connectToDatabase() {
-//      if(cached.conn) return cached.conn; 
-
-//      cached.conn = await mongoose.connect(MONGODB_URI);
-         
-
-//      return cached.conn; 
-// }
-
-
-
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
