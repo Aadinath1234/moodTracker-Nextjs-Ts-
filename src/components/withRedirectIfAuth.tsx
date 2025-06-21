b425b2ab13d552649821ea1f79e2ba2fa3a696b2
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-06-21 17:31:33 +0530

    Update withRedirectIfAuth.tsx

--- src/components/withRedirectIfAuth.tsx
+++ src/components/withRedirectIfAuth.tsx
@@ -1,29 +1,3 @@
-// import React, {useEffect} from "react";
-// import {useRouter} from 'next/router'; 
-
-
-// const withRedirectIfAuth = (WrappedComponent: React.ComponentType) => {
-//      const RedirectIfAuth = (props: any) =>{
-//          const router = useRouter();
-
-//          useEffect(() =>{
-//              const user = JSON.parse(localStorage.getItem('user') || '{}');
-//              if(user && user.token){
-//                 router.replace('/Home'); 
-//              }
-//          },[]); 
-
-//          return <WrappedComponent {...props} />; 
-//      };
-//  return RedirectIfAuth;     
-// };
-
-// export default withRedirectIfAuth;
-
-
-
-
-
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
