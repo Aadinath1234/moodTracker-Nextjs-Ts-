eab3205259ee6af1d45d1f47f7a0c90b13a33edf
Aadinath1234 <93751167+Aadinath1234@users.noreply.github.com>
2025-06-21 14:38:14 +0530

    Update realApi.ts

--- src/pages/api/realApi.ts
+++ src/pages/api/realApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const realApi = axios.create({
     //   baseURL: 'https://localhost:3000/api',
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',  
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://mood-trackerr.vercel.app/api',  
 });
 
 export default realApi; 
